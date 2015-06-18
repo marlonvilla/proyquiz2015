1bc5ac4779713e19860c22dcac47551200d643c5
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-18 17:02:50 -0430

    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require("../controllers/quiz_controller");
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+
+router.use('/quizes/question',quizController.question);
+router.use('/quizes/answer',quizController.answer);
+
 
 module.exports = router;
