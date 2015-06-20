df8c9298cc56b0169385d1481e1a106454099761
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-20 17:28:35 -0430

    sequelize y sqlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,21 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req,res){
-	res.render('quizes/question',{pregunta:"¿Capital de Italia? "});
+	models.Quiz.findAll().then(function(quiz) {
+	res.render('quizes/question',{pregunta: quiz[0].pregunta})
+	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req,res){
-	if ( req.query.respuesta === "Roma" ) {
+	models.Quiz.findAll().then(function(quiz) {
+	if (req.query.respuesta === quiz[0].respuesta ) {
 		res.render('quizes/answer',{respuesta: "Correcto"});
-} else {
+	} else {
 	   res.render('quizes/answer',{respuesta: "Incorrecto"});
-}
+	}
+	})
 };
 
 // GET /author
