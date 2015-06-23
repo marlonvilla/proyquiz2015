2b215b2ef7ea98e5ad68e3cf1b42bfcd7fda087b
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-23 09:07:29 -0430

    autoload y control de errores

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
 });
 
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
