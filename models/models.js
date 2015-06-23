2f0fca146dff98ffc5b36b11d2317114ebad53cc
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-23 05:54:38 -0430

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -18,7 +18,7 @@ var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgress
 var sequelize = new Sequelize(DB_name, user, pwd, 
-	{	dialect: 	protocol, 
+	{	dialect: 	dialect, 
 		protocol: 	protocol,
 		port:			port,
 		host:			host,
@@ -35,11 +35,14 @@ exports.Quiz = Quiz; // Exporta definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-   // success(..) ejecuta el manejador una vez creada la tabla
+   // then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count) {
 	if(count === 0) {   // la tabla se inicializa solo si está vacía
 		Quiz.create({  pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
+						});
+		Quiz.create({  pregunta: 'Capital de portugal',
+							respuesta: 'Lisboa'
 						})
 		.then(function() {console.log('Base de Datos Inicializada')});
 	};
