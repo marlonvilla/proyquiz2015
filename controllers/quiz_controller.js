9401c231eba000fe2d0676a67cf74e1af0114dbe
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-24 09:29:54 -0430

    p2p modluo 7 formulario de busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,12 +14,23 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(
-	function(quizes){
-		res.render('quizes/index.ejs', {quizes:quizes});
-	}).catch(function(error) {next(error);})
+	console.log("Valor de search: "+req.query.search);
+	if ( !req.query.search ) {
+		models.Quiz.findAll().then(
+		function(quizes){
+			res.render('quizes/index.ejs', {quizes:quizes});
+		}).catch(function(error) {next(error);})
+	} else
+	{
+		console.log("Haría busquedad según formulario");
+		var search = '%' + (req.query.search).replace(/ /g,'%') + '%';
+		models.Quiz.findAll({where:['pregunta like?', search ]}).then(
+		function(quizes){
+			res.render('quizes/index.ejs', {quizes:quizes});
+		}).catch(function(error) {next(error);})
+	}		
 };
-
+	
 // GET /quizes/:id
 exports.show = function(req,res){
 	res.render('quizes/show',{quiz: req.quiz});
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+
 router.get('/author',quizController.author);
 
 
