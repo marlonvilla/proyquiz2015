e7e67e2ce912d1da122a0fc6d2fb6bea95a28fde
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-26 14:40:23 -0430

    p2p modulo 8 agregar campo a DB y <select

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,7 +47,7 @@ exports.answer = function(req,res){
 // GET /quizes/new
 exports.new = function(req,res){
 	var quiz = models.Quiz.build(    // Crea objeto quiz
-		{pregunta: "Pregunta", respuesta:"Respuesta"}
+		{pregunta: "Pregunta", respuesta:"Respuesta", tema: "otro"}
 	);
 	res.render('quizes/new', {quiz:quiz, errors:[]});
 }
@@ -61,7 +61,7 @@ exports.create = function(req,res){
 			res.render('quizes/new', {quiz: quiz, errors:err.errors});
 		} else {
 			// guarda en DB los campos pregunta y respuesta de quiz
-			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 			res.redirect('/quizes')})
 		}
 	}
@@ -78,13 +78,14 @@ exports.edit = function(req,res) {
 exports.update = function(req,res){
 	req.quiz.pregunta  = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema      = req.body.quiz.tema;
 	
 	req.quiz.validate().then(function(err){
 		if (err) {
 			res.render('quizes/edit', {quiz:req.quiz, errors:err.errors});
 		} else {
 			req.quiz   // save: guarda campos pregunta y respuesta en DB
-			.save( {fields: ["pregunta", "respuesta"]})
+			.save( {fields: ["pregunta", "respuesta", "tema"]})
 			.then( function(){ res.redirect('/quizes');});
 		}
 	});
