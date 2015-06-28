b9c9ea06f05a71ba103b231edf564cdf20226db3
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-28 13:12:42 -0430

    Agregar Tabla comentarios

--- models/models.js
+++ models/models.js
@@ -31,7 +31,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importa definición de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+// Establecer las relaciones
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // Exporta definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
