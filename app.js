a7ca58219352ff17143691dd663996d0e5620f2e
Marlon Villamizar <villamizarmj@gmail.com>
2015-06-29 02:16:08 -0430

    Autentificación y Autorizacion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var partials = require("express-partials");
 var methodOverride = require('method-override');
 var routes = require('./routes/index');
-
+var session = require('express-session');
 var app = express();
 
 
@@ -22,9 +22,21 @@ app.use(partials());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res, next) {
+// guardar path en session.redir para después de login
+	if (!req.path.match(/\/login|\/logout/)) {
+		req.session.redir = req.path;
+	}
+
+// Hacer visible req.session en las vistas
+	res.locals.session = req.session;
+	next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require("../controllers/quiz_controller");
 var commentController = require("../controllers/comment_controller");
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de las rutas de sesión
+router.get('/login', sessionController.new);      // Formulario login
+router.post('/login', sessionController.create);  // Crear sesión
+router.get('/logout', sessionController.destroy); // Destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',quizController.index);
